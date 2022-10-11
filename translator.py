7d7ed9c49ffd12ae02b06c4800de243edead1949
Nikita <115578029+SamMalor@users.noreply.github.com>
2022-10-11 20:36:14 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,15 +3,15 @@ from mtranslate import translate
 import time
 from datetime import datetime
 
-file=input('введите название файла  ')+'.xlsx'
+file=input('write name of the file  ')+'.xlsx'
 wb = openpyxl.load_workbook(file)
-sheet1 = wb[input('введите название листа  ')]
+sheet1 = wb[input('write name of the list  ')]
 count=0
-d=int(input('введите номер строки, с которой начать '))
+d=int(input('write number of the first line '))
 A='D'+str(d)
-B='D'+input('введите номер последней строки ')
-r= 14900 #размер перевода за раз
-print('программа начала переводить', datetime.now())
+B='D'+input('write number of the last line ')
+r= 14900 #tranlate range
+print('program start translating', datetime.now())
 for i in sheet1[A:B]:
     for cell in i:
         if count <= r:
@@ -23,9 +23,9 @@ for i in sheet1[A:B]:
             print(datetime.now())
             r += 14900
 wb.save(file)
-print("перевод завершен")
-print('количество переведенных символов', count)
-print('программа завершила перевод в', datetime.now())
+print("translate is finished", datetime.now())
+print('number of transleted symbols', count)
+
 
 
 
